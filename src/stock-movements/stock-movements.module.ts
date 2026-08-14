71652c53c8b7728b42019bbb9628e25c3faff169
Facundo <facualvarezt@gmail.com>
2026-08-13 21:54:12 -0300

    fix: trigger low stock alerts from movements

--- src/stock-movements/stock-movements.module.ts
+++ src/stock-movements/stock-movements.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationSettingsModule } from '../notification-settings/notification-settings.module';
 import { StockMovement } from './entities/stock-movement.entity';
 import { StockMovementRepository } from './repositories/stock-movement.repository';
 import { StockMovementsController } from './stock-movements.controller';
 import { StockMovementsService } from './stock-movements.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StockMovement])],
+  imports: [TypeOrmModule.forFeature([StockMovement]), NotificationSettingsModule],
   controllers: [StockMovementsController],
   providers: [StockMovementsService, StockMovementRepository],
 })
